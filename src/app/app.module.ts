54d9c6fb259155a5668fc76f3c2ff130e9fdde39
KilliBruhh <61047380+KilliBruhh@users.noreply.github.com>
2022-04-13 20:32:28 +0200

    Home Page
    - Added Header
    - Home page made
    - Added Footer
    - closed issue #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    HomeComponentComponent,
+    FooterComponentComponent
   ],
   imports: [
     BrowserModule
